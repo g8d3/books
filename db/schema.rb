276e6082c93b4428595c9f4183ca5d5d3b25b28f
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-09-28 12:32:14 -0500

    Allow to select author.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130928165248) do
+ActiveRecord::Schema.define(version: 20130928165855) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20130928165248) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "author_id"
   end
 
 end
